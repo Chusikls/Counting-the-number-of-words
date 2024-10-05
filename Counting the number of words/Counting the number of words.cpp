3f45e43bf551f749a7506a489d050c9d0a5717fc
Chusikls <Chusikl@yandex.ru>
2024-10-05 12:19:11 +0300

    Добавьте файлы проекта.

---
+++ Counting the number of words/Counting the number of words.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    int word = 0;
+    std::string str;
+    std::cout << "Введите строку: \n";
+    std::getline(std::cin, str);
+
+    for (int i = 0; str[i] > str.length(); i++) {
+
+        if (str[i] >= '!' && str[i] <= '~' && (str[i + 1] == ' ' || str[i + 1] < str.length())) {
+            word++;
+        }       
+    }
+    std::cout << "Ответ: " << word << "\n";
+}
+
+
